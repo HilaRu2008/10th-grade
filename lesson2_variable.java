2a5634994e469cda6d28f90cf3d4830a6540114b
HilaRu2008 <144587689+HilaRu2008@users.noreply.github.com>
2023-09-10 11:26:49 +0300

    Create lesson2_variable.java

---
+++ lesson2_variable.java
@@ -0,0 +1,35 @@
+
+public class Main
+{
+public static void main(String[] args) {
+    
+    /*
+    int x=4, y=3,z=2;
+    System.out.println(x);
+    System.out.println(y);
+  System.out.println(z);
+  
+  
+  
+  
+  double x = 1.344;
+  int y = 30;
+  System.out.println(x + "    " + y);
+  
+  
+  char H = 'h', i='i', l='l' , a='a';
+  System.out.println(H+""+i+""+l+""+a);
+  
+
+  
+  
+  int x=1,y=5;
+  System.out.println(x+ " is lesser than " + y);
+  */
+  
+  
+  
+  char x='4';
+  System.out.println(x+"" + x);
+}
+}
