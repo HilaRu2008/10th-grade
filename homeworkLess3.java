a5e2f500f3f621d353b2aca51e0b49084b4d7d26
HilaRu2008 <144587689+HilaRu2008@users.noreply.github.com>
2023-09-19 16:28:30 +0300

    Create homeworkLess3.java

---
+++ homeworkLess3.java
@@ -0,0 +1,58 @@
+
+
+public class Main
+{
+  public static void main(String[] args) {
+
+    
+    int x=4, y=3,z=2;
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+  
+  
+  
+    
+    double x = 1.344;
+    int y = 30;
+    System.out.println(x + "    " + y);
+    
+    
+    char H = 'h', i='i', l='l' , a='a';
+    System.out.println(H+""+i+""+l+""+a);
+    
+    
+    
+    int x=1,y=5;
+    System.out.println(x+ " is lesser than " + y);
+ 
+
+		
+		int x =2 , y=3, z=4;
+		System.out.println((float)((x+y+z) / 3));
+		
+		
+		
+		
+		int a = 8 , b = 5;
+		System.out.println(Math.sqrt(a*a + b*b));
+		
+		
+		
+		
+		int x = 3;
+		System.out.println("y= "+(2*x +5));
+		
+		
+		
+		
+		String str1 = "I like to study computer science" , str2 = "and therefore ready to invest" , str3 = "hours a day for practice" ;  
+		
+		int hours = 3;
+		
+		System.out.println(str1 + " " + str2  + hours +   str3);
+
+
+
+  }
+}
